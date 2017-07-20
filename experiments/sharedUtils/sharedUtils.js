e3e7cb5018b94aa2d34acb65e96e56f074144abf
Andrew Jong <andrewjong87@gmail.com>
2017-07-20 14:54:21 -0700

    Move data folder into experiments because Heroku

--- experiments/sharedUtils/sharedUtils.js
+++ experiments/sharedUtils/sharedUtils.js
@@ -108,7 +108,7 @@ var getLongFormTime = function () {
 
 var establishStream = function(game, dataPoint) {
   var startTime = getLongFormTime();
-  var dirPath = ['..', 'data', game.expName, dataPoint.eventType].join('/');
+  var dirPath = ['data', game.expName, dataPoint.eventType].join('/');
   var fileName = startTime + "-" + game.id + ".csv";
   var filePath = [dirPath, fileName].join('/');
 
